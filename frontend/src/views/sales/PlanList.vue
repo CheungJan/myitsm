f3ced676c75abb6f8424c6ddad226f5b010ba358
CheungJan <cadence_zhang@hotmail.com>
2026-06-27 11:01:16 +0800

    refactor(sales): 预计划新建对话框四区布局+plantyp联动+库存查询
    布局:
      简要信息: 计划类型+计划单号
      客户信息: 磁卡号/编码/名称/地址/联系人/电话/经理/类型/合同等14字段
      设备信息: 按plantyp+pos_from动态切换
        - 商用仓库(00): 机型选择+库存量+押金自动带入
        - 门店移机/烟草直调(01/02): 源设备+新客户+无效化
        - 旧机翻新(20)+IT/海晟(03/04): 旧设备信息
        - 取回(30): 机型+EID+无效化
        - 关门(40): 机型选择
        - 设备变更(10): 新磁卡号+新设备
      金额: 租赁/购买+押金
    
    pos_from 按plantyp过滤可用来源

--- frontend/src/views/sales/PlanList.vue
+++ frontend/src/views/sales/PlanList.vue
@@ -149,28 +149,91 @@
     </el-drawer>
 
     <!-- 新建/编辑对话框 -->
-    <el-dialog :title="isEdit?'编辑预计划':'新建预计划'" v-model="dialogVisible" width="560px">
-      <el-form :model="form" label-width="90px">
-        <el-form-item label="计划类型"><el-select v-model="form.plantyp" style="width:100%" @change="onPlantypChange"><el-option v-for="o in plOptions" :key="o.value" :label="o.label" :value="o.value"/></el-select></el-form-item>
-        <el-form-item label="客户名称"><el-input v-model="form.custnm"/></el-form-item>
-        <el-form-item label="磁卡号"><el-input v-model="form.custcard"/></el-form-item>
-        <el-form-item label="客户编码"><el-input v-model="form.custcd"/></el-form-item>
-        <el-form-item label="联系人"><el-input v-model="form.contactor"/></el-form-item>
-        <el-form-item label="电话"><el-input v-model="form.phoneno"/></el-form-item>
-        <el-form-item label="地址"><el-input v-model="form.address"/></el-form-item>
-        <el-form-item v-if="showPosFrom" label="设备来源"><el-select v-model="form.pos_from" style="width:100%" @change="onPosFromChange"><el-option v-for="o in pfOptions" :key="o.value" :label="o.label" :value="o.value"/></el-select></el-form-item>
-        <el-form-item label="租赁/购买"><el-radio-group v-model="form.is_rent"><el-radio value="Y">租赁</el-radio><el-radio value="N">购买</el-radio></el-radio-group></el-form-item>
-        <el-form-item v-if="showModelSelect" label="机型"><el-select v-model="form.pos_item" filterable clearable placeholder="选择机型" style="width:100%" @change="onModelSelect"><el-option v-for="m in modelOptions" :key="m.model_cd" :label="`${m.model_cd} ${m.model_nm}`" :value="m.model_cd"/></el-select></el-form-item>
-        <el-form-item v-if="!showModelSelect && showPosItem" label="机型"><el-input v-model="form.pos_item" placeholder="POS物料编码"/></el-form-item>
-        <el-form-item v-if="showPosid" label="设备EID"><el-input v-model="form.posid" placeholder="设备EID"/></el-form-item>
-        <el-form-item label="押金金额"><el-input-number v-model="form.deposit" :min="0" :precision="2" style="width:100%" controls-position="right"/></el-form-item>
-        <el-form-item label="运营类型"><el-input v-model="form.yun_type" placeholder="运营类型编码"/></el-form-item>
-        <el-form-item v-if="showNewFields" label="新磁卡号"><el-input v-model="form.new_custcard"/></el-form-item>
-        <el-form-item v-if="showNewFields" label="新客户编码"><el-input v-model="form.new_custcd"/></el-form-item>
-        <el-form-item v-if="showNewFields" label="新客户名称"><el-input v-model="form.new_custnm"/></el-form-item>
-        <el-form-item v-if="showNewFields" label="新地址"><el-input v-model="form.new_address"/></el-form-item>
-        <el-form-item v-if="showNewFields" label="新电话"><el-input v-model="form.new_phoneno"/></el-form-item>
-        <el-form-item v-if="showCustUseflg" label="门店无效"><el-switch v-model="form.cust_useflg" active-value="1" inactive-value="0" active-text="是" inactive-text="否"/></el-form-item>
+    <el-dialog :title="isEdit?'编辑预计划':'新建预计划'" v-model="dialogVisible" width="700px">
+      <el-form :model="form" label-width="100px">
+        <!-- 一、简要信息 -->
+        <el-divider content-position="left">简要信息</el-divider>
+        <el-row :gutter="16">
+          <el-col :span="12"><el-form-item label="计划类型"><el-select v-model="form.plantyp" style="width:100%" @change="onPlantypChange"><el-option v-for="o in plOptions" :key="o.value" :label="o.label" :value="o.value"/></el-select></el-form-item></el-col>
+          <el-col v-if="isEdit" :span="12"><el-form-item label="计划单号"><el-input :model-value="form.planno" disabled/></el-form-item></el-col>
+        </el-row>
+
+        <!-- 二、客户信息 -->
+        <el-divider content-position="left">客户信息</el-divider>
+        <el-row :gutter="16">
+          <el-col :span="8"><el-form-item label="磁卡号"><el-input v-model="form.custcard" placeholder="磁卡号"/></el-form-item></el-col>
+          <el-col :span="8"><el-form-item label="客户编码"><el-input v-model="form.custcd" placeholder="编码"/></el-form-item></el-col>
+          <el-col :span="8"><el-form-item label="客户名称"><el-input v-model="form.custnm" placeholder="名称"/></el-form-item></el-col>
+        </el-row>
+        <el-row :gutter="16">
+          <el-col :span="12"><el-form-item label="地址"><el-input v-model="form.address" placeholder="地址"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="联系人"><el-input v-model="form.contactor"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="电话"><el-input v-model="form.phoneno"/></el-form-item></el-col>
+        </el-row>
+        <el-row :gutter="16">
+          <el-col :span="6"><el-form-item label="经理"><el-input v-model="form.jl_contactor"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="经理电话"><el-input v-model="form.jl_phoneno"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="客户类型"><el-input v-model="form.classcd"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="业务类型"><el-input v-model="form.busityp"/></el-form-item></el-col>
+        </el-row>
+        <el-row :gutter="16">
+          <el-col :span="6"><el-form-item label="合同"><el-select v-model="form.is_contract"><el-option label="否" value="0"/><el-option label="是" value="1"/></el-select></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="品牌"><el-input v-model="form.pptcode" placeholder="品牌编码"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="通讯方式"><el-input v-model="form.commmode"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="运营类型"><el-input v-model="form.yun_type"/></el-form-item></el-col>
+        </el-row>
+
+        <!-- 三、设备信息 -->
+        <el-divider content-position="left">设备信息</el-divider>
+        <el-row v-if="showPosFrom" :gutter="16">
+          <el-col :span="8"><el-form-item label="设备来源"><el-select v-model="form.pos_from" style="width:100%" @change="onPosFromChange"><el-option v-for="o in filteredPfOptions" :key="o.value" :label="o.label" :value="o.value"/></el-select></el-form-item></el-col>
+        </el-row>
+        <!-- 商用仓库(00): 机型选择 + 库存 -->
+        <el-row v-if="isPosFrom('00')" :gutter="16">
+          <el-col :span="10"><el-form-item label="计划机型"><el-select v-model="form.pos_item" filterable clearable placeholder="选择机型" style="width:100%" @change="onModelSelect"><el-option v-for="m in modelOptions" :key="m.model_cd" :label="`${m.model_cd} ${m.model_nm}`" :value="m.model_cd"/></el-select></el-form-item></el-col>
+          <el-col :span="4"><el-form-item label="库存"><el-input :model-value="stockQty" disabled/></el-form-item></el-col>
+        </el-row>
+        <!-- 门店移机/烟草直调(01/02): 源设备信息 + 新客户 + 无效化 -->
+        <template v-if="isPosFrom('01') || isPosFrom('02')">
+          <el-row :gutter="16">
+            <el-col :span="6"><el-form-item label="源磁卡号"><el-input v-model="form.custcard"/></el-form-item></el-col>
+            <el-col :span="6"><el-form-item label="源设备ID"><el-input v-model="form.posid"/></el-form-item></el-col>
+            <el-col :span="6"><el-form-item label="机型"><el-input v-model="form.pos_item"/></el-form-item></el-col>
+            <el-col :span="6"><el-form-item label="门店无效"><el-switch v-model="form.cust_useflg" active-value="1" inactive-value="0"/></el-form-item></el-col>
+          </el-row>
+          <el-row :gutter="16">
+            <el-col :span="6"><el-form-item label="新磁卡号"><el-input v-model="form.new_custcard"/></el-form-item></el-col>
+            <el-col :span="6"><el-form-item label="新客户编码"><el-input v-model="form.new_custcd"/></el-form-item></el-col>
+            <el-col :span="6"><el-form-item label="新电话"><el-input v-model="form.new_phoneno"/></el-form-item></el-col>
+            <el-col :span="6"><el-form-item label="新地址"><el-input v-model="form.new_address"/></el-form-item></el-col>
+          </el-row>
+        </template>
+        <!-- 旧机翻新 pos_from=03/04: 旧设备信息 -->
+        <el-row v-if="form.plantyp==='20' && (isPosFrom('03')||isPosFrom('04'))" :gutter="16">
+          <el-col :span="6"><el-form-item label="旧设备ID"><el-input v-model="form.posid"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="旧机型"><el-input v-model="form.pos_item"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="设备来源"><el-input :model-value="pfMap[form.pos_from]" disabled/></el-form-item></el-col>
+        </el-row>
+        <!-- 取回/关门(30/40): 机型选择 -->
+        <el-row v-if="['30','40'].includes(form.plantyp)" :gutter="16">
+          <el-col :span="10"><el-form-item label="计划机型"><el-select v-model="form.pos_item" filterable clearable placeholder="选择机型" style="width:100%" @change="onModelSelect"><el-option v-for="m in modelOptions" :key="m.model_cd" :label="`${m.model_cd} ${m.model_nm}`" :value="m.model_cd"/></el-select></el-form-item></el-col>
+          <el-col v-if="form.plantyp==='30'" :span="6"><el-form-item label="设备EID"><el-input v-model="form.posid"/></el-form-item></el-col>
+          <el-col v-if="form.plantyp==='30'" :span="6"><el-form-item label="门店无效"><el-switch v-model="form.cust_useflg" active-value="1" inactive-value="0"/></el-form-item></el-col>
+        </el-row>
+        <!-- 设备变更(10): 新磁卡号+设备 -->
+        <el-row v-if="form.plantyp==='10'" :gutter="16">
+          <el-col :span="6"><el-form-item label="新磁卡号"><el-input v-model="form.new_custcard"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="新设备ID"><el-input v-model="form.new_posid"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="新机型"><el-input v-model="form.new_positem"/></el-form-item></el-col>
+          <el-col :span="6"><el-form-item label="新客户"><el-input v-model="form.new_custcd"/></el-form-item></el-col>
+        </el-row>
+
+        <!-- 四、租售金额 -->
+        <el-divider content-position="left">金额</el-divider>
+        <el-row :gutter="16">
+          <el-col :span="8"><el-form-item label="租赁/购买"><el-radio-group v-model="form.is_rent"><el-radio value="Y">租赁</el-radio><el-radio value="N">购买</el-radio></el-radio-group></el-form-item></el-col>
+          <el-col :span="8"><el-form-item label="押金金额"><el-input-number v-model="form.deposit" :min="0" :precision="2" style="width:100%" controls-position="right"/></el-form-item></el-col>
+        </el-row>
       </el-form>
       <template #footer><el-button @click="dialogVisible=false">取消</el-button><el-button type="primary" @click="handleSave" :loading="saving">保存</el-button></template>
     </el-dialog>
@@ -210,16 +273,27 @@ async function loadModels() {
   try { const r = await request.get<never,{data:any[]}>('/deposit/deposit-models'); modelOptions.value = (r?.data||[]).filter((m:any)=>m.useflg==='1') } catch { modelOptions.value = [] }
 }
 
-// 表单字段条件显隐（按 plantyp + pos_from 联动）
-const showPosFrom = computed(() => ['00','10','20'].includes(form.plantyp))
-const showPosItem = computed(() => { if (['30','40'].includes(form.plantyp)) return true; if (form.plantyp==='10') return false; if (form.plantyp==='20') return true; return form.pos_from!=='00' })
-const showPosid = computed(() => showPosItem.value)
-const showModelSelect = computed(() => modelOptions.value.length>0 && showPosItem.value)
-const showNewFields = computed(() => { if (form.plantyp==='10') return true; if (['00','20'].includes(form.plantyp)) return form.pos_from==='01'||form.pos_from==='02'; return false })
-const showCustUseflg = computed(() => { if (form.plantyp==='30') return true; if (form.plantyp==='10') return false; if (['00','20'].includes(form.plantyp)) return form.pos_from==='01'||form.pos_from==='02'; return false })
-function onPlantypChange() { form.pos_from=''; form.cust_useflg='0' }
-function onPosFromChange() { if (form.pos_from==='00') form.cust_useflg='0' }
-function onModelSelect(val: string) { const m = modelOptions.value.find(x=>x.model_cd===val); if (m) form.deposit = m.rent_money||0 }
+// pos_from 下拉选项（PB 按 plantyp 过滤可用来源）
+const plantypPfFilter: Record<string, string[]> = { '00': ['00','01','02'], '10': ['01'], '20': ['00','01','02','03','04'], '30': [], '40': [] }
+const filteredPfOptions = computed(() => pfOptions.value.filter(o => (plantypPfFilter[form.plantyp]||[]).includes(o.value)))
+const showPosFrom = computed(() => (plantypPfFilter[form.plantyp]||[]).length > 0)
+// 判断当前 pos_from
+function isPosFrom(v: string) { return form.pos_from === v }
+const stockQty = ref(0)
+
+function onPlantypChange() { form.pos_from = ''; form.cust_useflg = '0'; form.pos_item = ''; form.posid = ''; stockQty.value = 0 }
+function onPosFromChange() { if (form.pos_from === '00') { form.cust_useflg = '0'; form.new_custcard = ''; form.new_custcd = ''; form.new_phoneno = ''; form.new_address = '' } }
+async function onModelSelect(val: string) {
+  const m = modelOptions.value.find(x => x.model_cd === val)
+  if (m) form.deposit = m.rent_money || 0
+  // 查库存
+  if (val && form.plantyp) {
+    try {
+      const r = await request.get('/warehouse/stock', { params: { itemcd: val, whcd: '04', per_page: 1 } }) as any
+      stockQty.value = r?.data?.total_qty || r?.data?.items?.reduce((s: number, i: any) => s + Number(i.qty || 0), 0) || 0
+    } catch { stockQty.value = 0 }
+  }
+}
 
 // 状态标签映射 (对齐 PB plan_cust.status: 00/01/02/04/09)
 function statusLabel(s: string) {
